d15551088561565a199104202b6f320e0ef75f14
Fernando Fantinel <94275030+fernandofantinel@users.noreply.github.com>
2023-12-12 22:42:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do projeto https://www.tabnews.com.br, a partir das aulas do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Testando git amend e push</h1>
+  return <h1>Testando git amend e push</h1>;
 }
 
 export default Home;
