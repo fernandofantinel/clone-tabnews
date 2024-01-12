e1673caabd42181490f014d92b590104674db04c
Fernando Fantinel <94275030+fernandofantinel@users.noreply.github.com>
2024-01-12 12:10:42 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 2 as sum");
+  console.log(result.rows);
   return response.status(200).json({ chave: "são acima da média" });
 }
 
