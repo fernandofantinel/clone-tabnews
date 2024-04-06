80d06f73f8d878dd8a5ca34558a32f0e1897f2c7
Fernando Fantinel <94275030+fernandofantinel@users.noreply.github.com>
2024-04-06 20:33:47 +0000

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const dotEnv = require("dotenv");
+dotEnv.config({
+  path: ".env.development",
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
